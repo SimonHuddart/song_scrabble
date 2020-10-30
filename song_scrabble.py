65f905d397cd83877e6ba193d632bf8059c46f2a
Simon Huddart <huddart.simon@gmail.com>
2020-10-30 17:40:07 -0400

    added statistics and have working CLI

--- song_scrabble.py
+++ song_scrabble.py
@@ -2,11 +2,12 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import re
-import pandas as pd
 
 GENIUS_API_TOKEN='N58t0vu51_G62y9vlsrxLwMwUNedUSiOgm1UHNL8HPx3GTMdVysiuu1MySRfSUBA'
 
 letter_score = {"A":1,"B":3,"C":3,"D":2,"E":1,"F":4,"G":2,"H":4,"I":1,"J":8,"K":5,"L":1,"M":3,"N":1,"O":1,"P":3,"Q":10,"R":1,"S":1,"T":1,"U":1,"V":4,"W":4,"X":8,"Y":4,"Z":10}
+
+punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
 
 scrab_dict = open("scrab_dict.txt")
 scrab_dict = scrab_dict.read().split()
@@ -49,9 +50,15 @@ def request_song_url(artist_name, song_cap):
     
 # Scrape lyrics from a Genius.com song URL
 def scrape_song_lyrics(url):
-    page = requests.get(url)
-    html = BeautifulSoup(page.text, 'html.parser')
-    lyrics = html.find('div', class_='lyrics').get_text()
+    while True:
+        try:
+            print("Trying to scrape: " + url + "...")
+            page = requests.get(url)
+            html = BeautifulSoup(page.text, 'html.parser')
+            lyrics = html.find('div', class_='lyrics').get_text()
+        except AttributeError:
+            continue
+        break
     #remove identifiers like chorus, verse, etc
     lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
     #remove empty lines
@@ -59,13 +66,13 @@ def scrape_song_lyrics(url):
     return lyrics
 
 def write_lyrics_to_file(artist_name, song_count):
-    f = open('lyrics/' + artist_name.lower() + '.txt', 'wb')
+    f = open('lyrics.txt', 'wb')
     urls = request_song_url(artist_name, song_count)
     for url in urls:
         lyrics = scrape_song_lyrics(url)
         f.write(lyrics.encode("utf8"))
     f.close()
-    num_lines = sum(1 for line in open('lyrics/' + artist_name.lower() + '.txt', 'rb'))
+    num_lines = sum(1 for line in open('lyrics.txt', 'rb'))
     print('Wrote {} lines to file from {} songs'.format(num_lines, song_count))
   
 def is_legal(word):
@@ -78,34 +85,85 @@ def scrabble_score(word):
     score = 0
     letters = list(word)
     for letter in letters:
-       score = word_score + letter_score[letter]
+       score = score + letter_score[letter]
     return score
 
 def avg_scrabble_score(text_file):
-    avg_score=0
-    for line in text:
-        line_score=0
+    total_score=0
+    word_count=0
+    legal_word_count=0
+    illegal_word_count=0
+    best_word = ('a',1)
+
+    for line in text_file:
+        line = line.upper()
         line_list = line.split()
+
         for word in line_list:
-            word_score=0
-            word = word.upper()
+            word_count += 1
+            word_score = 0
+          
+            for char in word:
+                if char in punc:
+                    word = word.replace(char,"")
+
             if is_legal(word) == True:
                 word_score = scrabble_score(word)
+                total_score = total_score + word_score
+                legal_word_count += 1
+
+                if word_count == 1:
+                    best_word = (word, word_score)
+                elif word_score > int(best_word[1]):
+                    best_word = (word, word_score)
+                else:
+                    pass
+
             else:
-                line_list.remove(word)
-            line_score = line_score + word_score
-        line_count = len(line_list)
-        avg_score = avg_score + line_score/line_count
-    return avg_score
+                illegal_word_count += 1
 
+    avg_score = total_score/word_count 
 
+    return (total_score, avg_score, word_count, legal_word_count, illegal_word_count, best_word[0], best_word[1])
 
-print (is_legal("APPLE"))
-avg_scrabble_score("APPLE APPLE")
+def song_scrabble(artist = "The Beatles", song_count = 10, file_dir = ('lyrics.txt')):
 
+    print("Enter Artist Name:")
+    artist = input()
+    if artist == '':
+        artist = "The Beatles"
+
+    print("Enter Number of Songs to Score:")
+    song_count = input()
+    if song_count == '':
+        song_count = 5
+    song_count = int(song_count)
+
+    write_lyrics_to_file(artist, song_count)
+    with open(file_dir, 'r') as file:
+        stats = avg_scrabble_score(file)
+        print("\n \n \n")
+        print("Total Scrabble Score = " + str(stats[0]))
+        print("Average Scrabble Score = " + str(stats[1]))
+        print("Total Word Count = " + str(stats[2]))
+        print("Percent Legal Words = " + str(stats[3]/stats[2]*100))
+        print("Percent Illegal Words = " + str(stats[4]/stats[2]*100))
+        print("Best Word and Best Word Score = " + stats[5] + " : " + str(stats[6]))
+
+song_scrabble()
+
+# DEMO
+
+#print (is_legal("APPLE"))
+#print (avg_scrabble_score("APPLE ORANGE \n GRAPE PEAR"))
+
+# DEMO
+
+#with open("lyrics.txt", 'r') as file:
+#    print(avg_scrabble_score(file))
 
 # DEMO  
-#write_lyrics_to_file('Eminem', 10)
+#write_lyrics_to_file('Eminem', 2)
 
 # DEMO
 #print(scrape_song_lyrics('https://genius.com/Lana-del-rey-young-and-beautiful-lyrics'))
